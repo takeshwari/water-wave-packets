c598f4d1b938290b33f7eb5710b20e8205500949
Takeshwari <kamalt4@students.rowan.edu>
2018-04-30 07:19:00 -0400

    Update SPHParticles.h

--- SimpleSampleDirectXTK/SPHParticles.h
+++ SimpleSampleDirectXTK/SPHParticles.h
@@ -25,19 +25,19 @@ const static float REST_DENS = 1.f; // rest density
 const static float GAS_CONST = 2000.f; // const for equation of state
 const static float H = 16.f; // kernel radius
 const static float HSQ = H*H; // radius^2 for optimization
-const static float MASS = 65.f; // assume all particles have the same mass
+const static float MASS = 90.f; // assume all particles have the same mass
 const static float VISC = 250.f; // viscosity constant
 const static float DT = 0.016f; // integration timestep
 
 const static float SPLASH_LIFETIME = 1.f;
-								 // smoothing kernels defined in Müller and their gradients
+								 // smoothing kernels defined in MÃ¼ller and their gradients
 const static float POLY6 = 315.f / (65.f*M_PI*pow(H, 9.f));
 const static float SPIKY_GRAD = -45.f / (M_PI*pow(H, 6.f));
 const static float VISC_LAP = 45.f / (M_PI*pow(H, 6.f));
 
 // simulation parameters
 const static float EPS = H; // boundary epsilon
-const static float BOUND_DAMPING = -0.5f;
+const static float BOUND_DAMPING = -0.6f;
 
 const static int DAM_PARTICLES = 500;
 
